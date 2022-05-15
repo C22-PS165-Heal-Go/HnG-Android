581188266c516cebc22c89289711c9a87d11dc3b
WayanPande <yande554@gmail.com>
2022-05-15 16:59:12 +0800

    add login fragment, styling, and viewmodel

--- app/src/main/java/com/example/heal_go/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/heal_go/ui/auth/LoginFragment.kt
@@ -1,19 +1,98 @@
 package com.example.heal_go.ui.auth
 
+import android.os.Build
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.heal_go.R
+import com.example.heal_go.databinding.FragmentLoginBinding
+import com.example.heal_go.databinding.FragmentWelcomeBinding
+import com.example.heal_go.ui.auth.viewmodel.AuthViewModel
+import com.wajahatkarim3.easyvalidation.core.Validator
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    private val authViewModel by viewModels<AuthViewModel>()
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        setupView()
+
+//        binding.etEmail.doOnTextChanged { text, start, before, count ->
+//            val validator = Validator(text.toString())
+//                .nonEmpty()
+//                .validEmail()
+//                .check()
+//            if (!validator) {
+//                binding.etEmail.error = "Email is not valid!"
+//            }else {
+//                binding.etEmail.error = null
+//            }
+//        }
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            val emailValidator = Validator(email)
+                .nonEmpty()
+                .validEmail()
+                .check()
+
+            val passwordValidator = Validator(password)
+                .nonEmpty()
+                .minLength(8)
+                .atleastOneNumber()
+                .check()
+
+            if (emailValidator && passwordValidator) {
+                authViewModel.userLoginHandler(email, password)
+                Toast.makeText(activity, "Data received", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(activity, "Please check on your credentials", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            activity?.moveTaskToBack(true)
+            activity?.finish()
+        }
+
+
+        return binding.root
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requireActivity().window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            requireActivity().window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/heal_go/ui/auth/viewmodel/AuthViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.heal_go.ui.auth.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+    fun userLoginHandler(email: String, password: String) {}
+}
