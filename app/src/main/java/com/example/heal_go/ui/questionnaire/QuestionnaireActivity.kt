edf4162ab902a26a28d21c1b2d2d0f5964dcd8a2
WayanPande <yande554@gmail.com>
2022-05-25 07:22:51 +0800

    Add dialog when user finish the questionnaire

--- app/src/main/java/com/example/heal_go/ui/questionnaire/QuestionnaireActivity.kt
+++ app/src/main/java/com/example/heal_go/ui/questionnaire/QuestionnaireActivity.kt
@@ -1,16 +1,23 @@
 package com.example.heal_go.ui.questionnaire
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.heal_go.R
 import com.example.heal_go.databinding.ActivityQuestionnaireBinding
+import com.example.heal_go.ui.dashboard.DashboardActivity
 import com.example.heal_go.ui.onboarding.adapter.OnboardingPagerAdapter
 import com.example.heal_go.ui.questionnaire.questions.*
 import com.example.heal_go.ui.questionnaire.viewmodel.QuestionnaireViewModel
@@ -29,24 +36,8 @@ class QuestionnaireActivity : AppCompatActivity() {
 
         val onBoardingPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
-
-                when (position) {
-                    0 -> {
-                        binding.backBtn.visibility = View.INVISIBLE
-                        binding.finishBtn.visibility = View.GONE
-                        binding.nextBtn.visibility = View.VISIBLE
-                    }
-                    5 -> {
-                        binding.backBtn.visibility = View.VISIBLE
-                        binding.finishBtn.visibility = View.VISIBLE
-                        binding.nextBtn.visibility = View.GONE
-                    }
-                    else -> {
-                        binding.backBtn.visibility = View.VISIBLE
-                        binding.finishBtn.visibility = View.GONE
-                        binding.nextBtn.visibility = View.VISIBLE
-                    }
-                }
+                setNextButton(position)
+                setEnabledButton(position)
             }
         }
 
@@ -81,12 +72,58 @@ class QuestionnaireActivity : AppCompatActivity() {
             }
 
             finishBtn.setOnClickListener {
-                Toast.makeText(this@QuestionnaireActivity, "FINISH", Toast.LENGTH_SHORT).show()
+                showDialog()
             }
         }
 
         questionnaireViewModel.quesionnaireAnswer.observe(this) {
             Log.d("REQBODY", it.toString())
+        }
+    }
+
+    private fun setEnabledButton(position: Int) {
+        questionnaireViewModel.quesionnaireAnswer.observe(this@QuestionnaireActivity) {
+            when (position) {
+                0 -> {
+                    binding.nextBtn.isEnabled = it.question1 != null
+                }
+                1 -> {
+                    binding.nextBtn.isEnabled = it.question2 != null && it.question2.isNotEmpty()
+                }
+                2 -> {
+                    binding.nextBtn.isEnabled = it.question3 != null && it.question3.isNotEmpty()
+                }
+                3 -> {
+                    binding.nextBtn.isEnabled = it.question4 != null
+                }
+                4 -> {
+                    binding.nextBtn.isEnabled = it.question5 != null
+                }
+                5 -> {
+                    binding.finishBtn.isEnabled = it.question6 != null
+                }
+
+            }
+        }
+    }
+
+    private fun setNextButton(position: Int) {
+        when (position) {
+            0 -> {
+                binding.backBtn.visibility = View.INVISIBLE
+                binding.finishBtn.visibility = View.GONE
+                binding.nextBtn.visibility = View.VISIBLE
+            }
+            5 -> {
+                binding.backBtn.visibility = View.VISIBLE
+                binding.finishBtn.visibility = View.VISIBLE
+                binding.nextBtn.visibility = View.GONE
+            }
+            else -> {
+                binding.backBtn.visibility = View.VISIBLE
+                binding.finishBtn.visibility = View.GONE
+                binding.nextBtn.visibility = View.VISIBLE
+            }
         }
     }
 
@@ -110,6 +147,34 @@ class QuestionnaireActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showDialog() {
+        val builder: AlertDialog.Builder =
+            AlertDialog.Builder(this@QuestionnaireActivity, R.style.WrapContentDialog)
+        val inflater: LayoutInflater = layoutInflater
+        builder.setView(inflater.inflate(R.layout.questionnaire_custom_dialog, null))
+        val dialog = builder.create()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        val btnProceed = dialog.findViewById<Button>(R.id.proceed_btn)
+        val btnCancel = dialog.findViewById<Button>(R.id.cancel_btn)
+        val btnClose = dialog.findViewById<ImageButton>(R.id.close_btn)
+
+        btnProceed?.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnClose?.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        btnCancel?.setOnClickListener {
+            dialog.dismiss()
         }
     }
 
