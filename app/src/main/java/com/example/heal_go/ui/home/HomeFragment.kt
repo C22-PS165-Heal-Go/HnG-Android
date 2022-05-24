19d2e18e52f339691197eae4868be2043b095761
WayanPande <yande554@gmail.com>
2022-05-24 19:31:10 +0800

    Add categories chip style and temporary recyclerview

--- app/src/main/java/com/example/heal_go/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/heal_go/ui/home/HomeFragment.kt
@@ -1,14 +1,15 @@
 package com.example.heal_go.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.heal_go.R
-import com.example.heal_go.databinding.FragmentFirstScreenBinding
+import androidx.core.view.children
+import androidx.fragment.app.Fragment
 import com.example.heal_go.databinding.FragmentHomeBinding
+import com.google.android.material.chip.Chip
 
 class HomeFragment : Fragment() {
 
@@ -20,6 +21,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.chipgroup.children.toList().filter { (it as Chip).isChecked }
+            .forEach { Log.d("COBA", it.toString()) }
 
         binding.startBtn.setOnClickListener {
             Toast.makeText(requireContext(), "Start now", Toast.LENGTH_SHORT).show()
