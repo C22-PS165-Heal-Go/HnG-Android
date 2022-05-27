d96d4de75254d126e6b8017ba437d3d7015fa29b
WayanPande <yande554@gmail.com>
2022-05-27 15:52:54 +0800

    Add save data to datastore on tutorial bottom sheet

--- app/src/main/java/com/example/heal_go/ui/recommendation/TutorialBottomSheet.kt
+++ app/src/main/java/com/example/heal_go/ui/recommendation/TutorialBottomSheet.kt
@@ -1,6 +1,8 @@
 package com.example.heal_go.ui.recommendation
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,12 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class TutorialBottomSheet : BottomSheetDialogFragment() {
     private var binding: RecommendationTutorialDialogBinding? = null
+    private lateinit var onActionClickListener: OnActionClickListener
+
+    /*define interface when action is clicked*/
+    interface OnActionClickListener {
+        fun onUnderstandBtnClickListener()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +32,24 @@ class TutorialBottomSheet : BottomSheetDialogFragment() {
 
         /*when not interested actions is clicked, this will be return not-interested actions to parent page*/
         binding?.understandBtn?.setOnClickListener {
+            onActionClickListener.onUnderstandBtnClickListener()
             this.dismiss()
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        /*initialize action click listener from parent fragment*/
+        try {
+            this.onActionClickListener = activity as OnActionClickListener
+        } catch (e: ClassCastException) {
+            Log.d(TutorialBottomSheet.TAG, e.message.toString())
+        }
+    }
+
+    companion object {
+        const val TAG = "TutorialBottomSheet"
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/heal_go/ui/recommendation/viewmodel/RecommendationViewModel.kt
+++ app/src/main/java/com/example/heal_go/ui/recommendation/viewmodel/RecommendationViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.heal_go.ui.recommendation.viewmodel
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.heal_go.data.repository.MainRepository
+import com.example.heal_go.data.repository.RecommendationTutorialRepository
+import kotlinx.coroutines.launch
 
-class RecommendationViewModel(provideMainRepository: MainRepository, context: Context) : ViewModel() {
+class RecommendationViewModel(
+    provideMainRepository: MainRepository,
+    recommendationTutorialRepository: RecommendationTutorialRepository,
+    context: Context
+) : ViewModel() {
+
+    private val tutorialRepository = recommendationTutorialRepository
+
+    fun onTutorialFinish() {
+        viewModelScope.launch {
+            tutorialRepository.saveToDataStore(true)
+        }
+    }
+
+    fun getTutorialDatastore(): LiveData<Boolean> {
+        return tutorialRepository.readFromDataStore.asLiveData()
+    }
 }
