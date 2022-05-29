bf59b48734b424c855c637173c7b26f8937d98c8
WayanPande <yande554@gmail.com>
2022-05-29 09:01:02 +0800

    Add searchbar, pill category, and destination recycle view

--- app/src/main/java/com/example/heal_go/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/example/heal_go/ui/discover/DiscoverFragment.kt
@@ -1,25 +1,60 @@
 package com.example.heal_go.ui.discover
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.heal_go.R
+import com.example.heal_go.databinding.FragmentDiscoverBinding
 import com.example.heal_go.ui.ViewModelFactory
 import com.example.heal_go.ui.dashboard.viewmodel.DashboardViewModel
 
 class DiscoverFragment : Fragment() {
 
-    private val dashboardViewModel by viewModels<DashboardViewModel> { ViewModelFactory(requireContext()) }
+    private val dashboardViewModel by viewModels<DashboardViewModel> {
+        ViewModelFactory(
+            requireContext()
+        )
+    }
+    private var _binding: FragmentDiscoverBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false)
+    ): View {
+        _binding = FragmentDiscoverBinding.inflate(inflater, container, false)
+
+        binding.svSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(requireContext(), query, Toast.LENGTH_SHORT).show()
+                view?.let { requireActivity().hideKeyboard(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+
+        })
+
+        return binding.root
     }
 
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
