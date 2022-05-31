46c301425bfce65fb931383130850a11e91eb6f0
WayanPande <yande554@gmail.com>
2022-05-31 20:49:25 +0800

    Add profile screen style and component

--- app/src/main/java/com/example/heal_go/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/heal_go/ui/profile/ProfileFragment.kt
@@ -5,21 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.example.heal_go.R
+import com.example.heal_go.databinding.FragmentHomeBinding
+import com.example.heal_go.databinding.FragmentProfileBinding
 import com.example.heal_go.ui.ViewModelFactory
 import com.example.heal_go.ui.dashboard.viewmodel.DashboardViewModel
 
 class ProfileFragment : Fragment() {
 
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
     private val dashboardViewModel by viewModels<DashboardViewModel> { ViewModelFactory(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            editBtn.setOnClickListener {
+                showWarningToast()
+            }
+            faqBtn.setOnClickListener {
+                showWarningToast()
+            }
+            languageBtn.setOnClickListener {
+                showWarningToast()
+            }
+            rateBtn.setOnClickListener {
+                showWarningToast()
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setProfileImage()
+    }
+
+    private fun setProfileImage() {
+        Glide.with(requireView())
+            .load("https://avatars.dicebear.com/api/adventurer-neutral/healgo.jpg")
+            .into(binding.profilePict)
+    }
+
+    private fun showWarningToast() {
+        Toast.makeText(requireContext(), "Sorry but this feature currently not available yet \uD83D\uDE14", Toast.LENGTH_SHORT).show()
     }
 
 }
