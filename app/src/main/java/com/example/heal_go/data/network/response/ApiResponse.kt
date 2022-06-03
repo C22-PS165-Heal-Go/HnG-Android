35f973aea188790de5417d056427427163269c14
Aldi Saepurahman <aldisaepurahman@gmail.com>
2022-06-03 19:18:31 +0700

    Memindahkan ApiResponse dalam satu file

--- app/src/main/java/com/example/heal_go/data/network/response/ApiResponse.kt
+++ app/src/main/java/com/example/heal_go/data/network/response/ApiResponse.kt
@@ -54,6 +54,38 @@ data class RegisterResponse(
 ) : Parcelable
 
 @Parcelize
+data class DestinationResponse(
+
+    @field:SerializedName("data")
+    val data: ArrayList<DestinationItem>? = null,
+
+    @field:SerializedName("success")
+    val success: Boolean = false,
+
+    @field:SerializedName("message")
+    val message: String? = null,
+
+    @field:SerializedName("code")
+    val code: Int? = null
+) : Parcelable
+
+@Parcelize
+data class DestinationItem(
+
+    @field:SerializedName("image")
+    val image: String? = null,
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("description")
+    val description: String? = null,
+
+    @field:SerializedName("location")
+    val location: String? = null
+) : Parcelable
+
+@Parcelize
 data class DateDiff(
     val seconds: Long,
     val minutes: Long,
